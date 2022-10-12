2e48fed7b2fa5c52df3f9c8c4f9c87bf7767ebf4
Jason Ling <Jasonling1996@gmail.com>
2022-10-12 00:05:48 +0000

    Added color to text over limit

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  // Grabbed the textbar from the DOM
+  const $tweet = $("#tweet-text");
+  const $counter = $("#counter");
+  const maxChar = 140;
+
+  // Change event
+  $tweet.keydown(function(event) {
+    const tweetLength = event.target.value.length;
+    const count = maxChar-tweetLength;
+    $counter.val(count)
+    if(count >= 0) {
+      $($counter).css("color", "black");
+    }
+    if(count < 0) {
+     $($counter).css("color", "red");
+    }
+  });
+}); 
