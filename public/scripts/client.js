8b0c23e955366179d27f2a3fe3934d2672d71a4c
Jason Ling <Jasonling1996@gmail.com>
2022-10-14 00:27:37 +0000

    Updated Error message from alert

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,21 +1,28 @@
 $(document).ready(function () {
 
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
   //Grab the form
   $form = $('.form-tweet');
   const $text = $('#tweet-text');
 
   // listen for the form to submit
   $form.on('submit', (event) => {
+
     // Stops browser from doing the default
     event.preventDefault();
     const textVal = $text.val();
-    console.log($text);
     if (textVal === '') {
-      return alert("Not Valid Entry, Try Again!");
+      return $('.error').text('❗ Not Valid Entry, Try Again! ❗').fadeIn('slow');
     }
     if (textVal.length > 140) {
-      return alert("Too Many Characters, Please Shorten");
+      return $('.error').text('❗ Too Many Characters, Please Shorten ❗').fadeIn('slow');
     }
+    $('.error').fadeOut('slow');
 
     // Get the data
     const dataToSendToServer = $form.serialize();
@@ -78,7 +85,7 @@ $(document).ready(function () {
           <p>${tweet.user.handle}</p>
         </div>
       </header>
-      <textarea name="text" class="fake-tweet-text">${tweet.content.text}</textarea>
+      <textarea name="text" class="fake-tweet-text">${escape(tweet.content.text)}</textarea>
       <footer class="fake-footer">
         <div>
           <p>${timeago.format(tweet.created_at)}</p>
